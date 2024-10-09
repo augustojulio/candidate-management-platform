d4e0e9c5df4ef1ddf9f1e51ce1468ba3470fe32a
augustojulio <augusto.juliogec@gmail.com>
2024-10-09 02:54:38 +0100

    chore: add CRUD routes

--- backend/candidates/urls.py
+++ backend/candidates/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from .views import CandidateViewSet, ClientViewSet, JobViewSet, RecruiterViewSet
+
+router = DefaultRouter()
+router.register(r'candidates', CandidateViewSet, basename='candidate')
+router.register(r'clients', ClientViewSet, basename='client')
+router.register(r'jobs', JobViewSet, basename='job')
+router.register(r'recruiters', RecruiterViewSet, basename='recruiter')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('candidates.urls')),
+    path('', include(router.urls)),
 ]
