b35604b73176d0d740e94db68f514b94a71f6ac0
augustojulio <augusto.juliogec@gmail.com>
2024-10-09 03:32:18 +0100

    chore: run migration and update tests

--- backend/candidates/tests.py
+++ backend/candidates/tests.py
@@ -1,8 +1,15 @@
-import pytest
+from django.test import TestCase
 from rest_framework.test import APIClient
+from .models import Client
 
-@pytest.mark.django_db
-def test_candidates_creation():
-    client = APIClient()
-    response = client.post('/api/candidates/', {'name': 'John Doe', 'email': 'johnd@gmail.com', 'phone': '11111111', 'status': 'active', 'job': 1, 'recruiter': 1})
-    assert response.status_code == 201
+class ClientTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.clientt = Client.objects.create(name="Test Client", email="aaa@aaa.com", phone="1234567890")
+
+    def test_candidate_creation(self):
+        self.assertEqual(self.clientt.name, "Test Client")
+
+    def test_get_candidates(self):
+        response = self.client.get('/api/clients/')
+        self.assertEqual(response.status_code, 200)
