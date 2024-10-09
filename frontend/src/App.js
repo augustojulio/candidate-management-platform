06f3c1494e50f4f22520fd6c01af2cac90e2348e
augustojulio <augusto.juliogec@gmail.com>
2024-10-09 11:57:33 +0100

    feat: create frontend routes and test frontend and backend together

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CandidateList from './components/CandidateList';
+import CandidateDetail from './components/CandidateDetail';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  console.log('App is rendering');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Route for Dashboard */}
+          <Route path="/dashboard" component={Dashboard} />
+
+          {/* Route for the list of candidates */}
+          <Route path="/candidates" exact component={CandidateList} />
+
+          {/* Route for candidate details (using candidate ID) */}
+          <Route path="/candidates/:id" component={CandidateDetail} />
+
+          {/* Default route */}
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
