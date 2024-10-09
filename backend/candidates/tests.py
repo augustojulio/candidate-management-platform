faee352c04070dce5c09dfdb645a8bdcc1690f1f
augustojulio <augusto.juliogec@gmail.com>
2024-10-09 14:44:01 +0100

    test: update unit tests with authentication

--- backend/candidates/tests.py
+++ backend/candidates/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 from .models import Client
 
 class ClientTests(TestCase):
     def setUp(self):
         self.client = APIClient()
+        
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        response = self.client.post('/api/token/', {'username': 'testuser', 'password': 'testpass'})
+        self.token = response.data['access']
+        
         self.clientt = Client.objects.create(name="Test Client", email="aaa@aaa.com", phone="1234567890")
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
 
     def test_candidate_creation(self):
         self.assertEqual(self.clientt.name, "Test Client")
